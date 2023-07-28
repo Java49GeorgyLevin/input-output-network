31ee462c6073f9c4199cd66c496fa1a67837f172
Georgy Levin <g.phyzy@gmail.com>
2023-07-28 18:01:32 +0300

    HW 32: displayDirContent - walk & list

--- src/telrean/io/InputOutputTest.java
+++ src/telrean/io/InputOutputTest.java
@@ -2,8 +2,12 @@ package telrean.io;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,27 +20,73 @@ class InputOutputTest {
 
 	@Test
 	void pathTests() {
-		Path pathParent = Path.of("../..");
-		System.out.println(pathParent.toAbsolutePath().normalize()
-				.getName(4));
+		Path pathParent = Path.of("./DIRS/DIR1");
+		System.out.println(pathParent
+				.toAbsolutePath()
+				.normalize()				
+				.getName(3)
+				);
 	}
+	
 	@Test
 	void displayDirContentTest() {
-		displayDirContent(Path.of("../.."), 3);
+		displayDirContent(Path.of("DIRS"), 3);
+	}
+	
+	@Test
+	void displayListDirContentTest() {
+		displayListDirContent(Path.of("DIRS"), 4);
 	}
 
 	private void displayDirContent(Path dirPath, int maxDepth) {
-		// TODO Auto-generated method stub
-		//Display content directory by using method walk of the class Files
-//		simple directory name>
-//       <simple file name> - file
-//       <subdirectory name> - dir
-//          <simple file name> - file
-//          <subdirectory name> - dir
-//                  
-//             <simple file name> - file
-
+		if(!Files.isDirectory(dirPath)) {
+			throw new IllegalArgumentException();
+		}
+		try {
+			Files.walk(dirPath, maxDepth)			
+			.forEach(p ->  {
+				int offset = p.getNameCount();				
+				System.out.printf("%s %s - %s\n", 
+						" ".repeat(offset * 4) , p.getFileName(), Files.isDirectory(p) ? "dir" : "file");
+			});
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			System.out.println("The path is not the path to a directory");
+		}
+	}
+	
+	
+	private void displayListDirContent(Path dirPath, int maxDepth) {
+		if(!Files.isDirectory(dirPath)) {
+			throw new IllegalArgumentException();			
+		}
 		
+		final int depth = --maxDepth;
+		int offset = dirPath.getNameCount();
+		printFiles(dirPath.getFileName(), offset, 'd');
+
+		if(maxDepth > 0) {		
+			try {
+				Files.list(dirPath)
+				.forEach(p -> { 
+					if(!Files.isDirectory(p)) {
+						printFiles(p.getFileName(), offset, 'f');						
+					} else {
+						displayListDirContent(p, depth);
+						} 
+					});
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private static void printFiles(Path fileName, int offset, Character dF) {
+		System.out.printf("%s %s - %s\n", " ".repeat(offset * 2), fileName,
+			dF.equals('d') ? "dir" : "file");	
 	}
 
 }
