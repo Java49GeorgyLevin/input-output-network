0575afd919537664afedb1bce7dfbc7f3dc146cc
Georgy Levin <g.phyzy@gmail.com>
2023-07-31 17:43:31 +0300

    CW #33 Byte oriented Streams

--- src/telrean/io/InputOutputTest.java
+++ src/telrean/io/InputOutputTest.java
@@ -2,91 +2,72 @@ package telrean.io;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.FilePermission;
 import java.io.IOException;
-import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
-import java.util.stream.Stream;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class InputOutputTest {
 
+	private static final Path FILE_PATH = Path.of("/Users/User/Documents/abc.txt");
+	private static final String NOT_EXIST = "doesn't exist";
+	private static final String NO_DIRECTORY = "no directory";
+
 	@BeforeEach
 	void setUp() throws Exception {
+		Files.createFile(FILE_PATH);
+	}
+	@AfterEach
+	void tearDown() throws Exception {
+		Files.delete(FILE_PATH);
 	}
 
 	@Test
 	void pathTests() {
-		Path pathParent = Path.of("./DIRS/DIR1");
-		System.out.println(pathParent
-				.toAbsolutePath()
-				.normalize()				
-				.getName(3)
-				);
-	}
-	
-	@Test
-	void displayDirContentTest() {
-		displayDirContent(Path.of("DIRS"), 3);
-	}
-	
-	@Test
-	void displayListDirContentTest() {
-		displayListDirContent(Path.of("DIRS"), 4);
+		Path pathParent = Path.of("../..");
+		System.out.println(pathParent.toAbsolutePath().normalize().getName(4));
 	}
 
-	private void displayDirContent(Path dirPath, int maxDepth) {
-		if(!Files.isDirectory(dirPath)) {
-			throw new IllegalArgumentException();
-		}
-		try {
-			Files.walk(dirPath, maxDepth)			
-			.forEach(p ->  {
-				int offset = p.getNameCount();				
-				System.out.printf("%s %s - %s\n", 
-						" ".repeat(offset * 4) , p.getFileName(), Files.isDirectory(p) ? "dir" : "file");
-			});
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			System.out.println("The path is not the path to a directory");
-		}
-	}
-	
-	
-	private void displayListDirContent(Path dirPath, int maxDepth) {
-		if(!Files.isDirectory(dirPath)) {
-			throw new IllegalArgumentException();			
-		}
+	@Test
+	void displayDirContentTest() throws IOException {
+		displayDirContent(Path.of(".."), 3);
+		assertThrowsExactly(IllegalArgumentException.class,
+				()->displayDirContent(Path.of("/Users/User/Doc"), 1), NOT_EXIST);
 		
-		final int depth = --maxDepth;
-		int offset = dirPath.getNameCount();
-		printFiles(dirPath.getFileName(), offset, 'd');
+		assertThrowsExactly(IllegalArgumentException.class,
+				()->displayDirContent(FILE_PATH, 1), NO_DIRECTORY);
+		
+	}
 
-		if(maxDepth > 0) {		
-			try {
-				Files.list(dirPath)
-				.forEach(p -> { 
-					if(!Files.isDirectory(p)) {
-						printFiles(p.getFileName(), offset, 'f');						
-					} else {
-						displayListDirContent(p, depth);
-						} 
-					});
-				
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+	private void displayDirContent(Path dirPath, int maxDepth) throws IOException {
+		// Display content directory by using method walk of the class Files
+//		simple directory name>
+//       <simple file name> - file
+//       <subdirectory name> - dir
+//          <simple file name> - file
+//          <subdirectory name> - dir
+//                  
+//             <simple file name> - file
+
+		dirPath = dirPath.toAbsolutePath().normalize();
+		if(!Files.exists(dirPath)) {
+			throw new IllegalArgumentException("doesn't exist");
 		}
-	}
-	
-	private static void printFiles(Path fileName, int offset, Character dF) {
-		System.out.printf("%s %s - %s\n", " ".repeat(offset * 2), fileName,
-			dF.equals('d') ? "dir" : "file");	
-	}
+		if (!Files.isDirectory(dirPath)) {
+			throw new IllegalArgumentException("no directory");
+		}
+		int count = dirPath.getNameCount();
+		try {
+			Files.walk(dirPath, maxDepth)
+					.forEach(p -> System.out.printf("%s%s - %s\n", " ".repeat(p.getNameCount() - count),
+							p.getName(p.getNameCount() - 1), Files.isDirectory(p) ? "dir" : "file"));
+		} catch (Exception e) {
+			
+		}
 
+	}
 }
