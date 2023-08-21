3a9f1c416f4105827285f04af2e8622acc852013
Georgy Levin <g.phyzy@gmail.com>
2023-08-21 21:53:05 +0300

    HW 39: Employees console application

--- src/telran/employees/controller/CompanyController.java
+++ src/telran/employees/controller/CompanyController.java
@@ -33,7 +33,7 @@ static Company company;
 				Item.of("Remove Employee", CompanyController::removeEmployeeItem),
 				Item.of("All Employees", CompanyController::getEmployeesItem),
 				Item.of("Data about Employee", CompanyController::getEmployeeItem),
-				Item.of(" Employees by Salary", CompanyController::getEmployeesBySalaryItem),
+				Item.of("Employees by Salary", CompanyController::getEmployeesBySalaryItem),
 				Item.of("Employees by Department", CompanyController::getEmployeesByDepartmentItem),
 				Item.of("Update salary", CompanyController::updateSalaryItem),
 				Item.of("Departments and Salary", CompanyController::getDepartmentSalaryDistributionItem),
@@ -61,38 +61,72 @@ static Company company;
 		
 		return LocalDate.now().minusYears(age);
 	}
+	
 	static void removeEmployeeItem(InputOutput io) {
-		//TODO
-	}
-	static void getEmployeeItem(InputOutput io) {
-		//TODO
-	}
-	static void getEmployeesItem(InputOutput io) {
-		//TODO
-	}
-	static void getDepartmentSalaryDistributionItem(InputOutput io) {
-		company.getDepartmentSalaryDistribution().forEach(io::writeLine);
-		
-	}
-	static void getSalaryDistributionItem(InputOutput io) {
-		//TODO
-	}
-	static void getEmployeesByDepartmentItem(InputOutput io) {
-		//TODO
-	}
-	static void getEmployeesBySalaryItem(InputOutput io) {
-		//TODO
-	}
-	static void getEmployeesByAgeItem(InputOutput io) {
-		//TODO
-	}
-	static void updateSalaryItem(InputOutput io) {
-		//TODO
-	}
-	static void updateDepartmentItem(InputOutput io) {
-		//TODO
+		long id = io.readLong("Enter Employee's ID to delete", "ID out range", MIN_ID, MAX_ID);
+		Employee empl = company.removeEmployee(id);
+		io.writeLine(empl == null ? "There's not an employee with the ID" : empl + " successfull deleted");
 	}
 	
+	static void getEmployeeItem(InputOutput io) {
+		long id = io.readLong("Enter ID", "ID out range", MIN_ID, MAX_ID);
+		Employee empl = company.getEmployee(id);
+		io.writeLine(empl == null ? "There's not an employee with the ID" : empl );
+	}
+	
+	static void getEmployeesItem(InputOutput io) {
+		company.getEmployees().forEach(io::writeLine);
+	}
+	
+	static void getDepartmentSalaryDistributionItem(InputOutput io) {
+		company.getDepartmentSalaryDistribution().forEach(io::writeLine);		
+	}
+	
+	static void getSalaryDistributionItem(InputOutput io) {
+		int interval = io.readInt("Enter interval", "Interval is wrong", 1 , Integer.MAX_VALUE);
+		company.getSalaryDistribution(interval).forEach(io::writeLine);
+	}
+	
+	static void getEmployeesByDepartmentItem(InputOutput io) {
+		String department = io.readString("Enter department", "There are not that department", departments);
+		company.getEmployeesByDepartment(department).forEach(io::writeLine);
+	}
+	
+	static void getEmployeesBySalaryItem(InputOutput io) {
+		int salaryFrom = io.readInt("Enter salary from", "Salary is wrong", MIN_SALARY, MAX_SALARY);
+		int salaryTo = io.readInt("Enter salary to", "Salary is wrong", salaryFrom, MAX_SALARY);
+		company.getEmployeesBySalary(salaryFrom, salaryTo).forEach(io::writeLine);
+	}
+	
+	static void getEmployeesByAgeItem(InputOutput io) {
+		int ageFrom = io.readInt("Enter age from", "The value of age is wrong", MIN_AGE, MAX_AGE);
+		int ageTo = io.readInt("Enter age to", "The value of age is wrong", ageFrom, MAX_AGE);
+		company.getEmployeesByAge(ageFrom, ageTo).forEach(io::writeLine);
+	}
+	
+	static void updateSalaryItem(InputOutput io) {
+		long id = io.readLong("Enter ID", "ID out range", MIN_ID, MAX_ID);
+		Employee empl = company.getEmployee(id);
+		if(empl == null) {
+			io.writeLine("There's not an employee with the ID");
+		} else {		
+			int salary = io.readInt("Enter new salary", "Wrong salary", MIN_SALARY, MAX_SALARY);
+			company.updateSalary(id, salary);
+			io.writeLine(empl + " salary successful updated to " + salary);
+		}
+	}
+	
+	static void updateDepartmentItem(InputOutput io) {
+		long id = io.readLong("Enter ID", "ID out range", MIN_ID, MAX_ID);
+		Employee empl = company.getEmployee(id);
+		if(empl == null) {
+			io.writeLine("There's not an employee with the ID");
+		} else {		
+			String department = io.readString("Enter department", "Wrong department", departments );
+			company.updateDepartment(id, department);
+			io.writeLine(empl + " department successful updated to " + department);
+		}
+	}	
 	
 
 }
