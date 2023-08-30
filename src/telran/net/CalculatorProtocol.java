08f61a596b9735f45da926e9c8af3cfae2b170b4
Georgy Levin <g.phyzy@gmail.com>
2023-08-30 17:52:07 +0300

    HW 41: Calculator TCP

--- src/telran/net/CalculatorProtocol.java
+++ src/telran/net/CalculatorProtocol.java
@@ -4,8 +4,27 @@ public class CalculatorProtocol implements ApplProtocol {
 
 	@Override
 	public Response getResponse(Request request) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+		Double[] datas = null;
+		Response response;
+		try {
+			datas = (Double[]) request.requestData();			
+		} catch (Exception e) {
+			new Response(ResponseCode.WRONG_DATA, e.getMessage());
+		};
+		if(datas.length != 2) {
+			response = new Response(ResponseCode.WRONG_DATA, request.requestData());
+		} else { 
+			response = switch(request.requestType()) {
+			case "add" -> new Response(ResponseCode.OK, datas[0] + datas[1]);
+			case "minus" ->  new Response(ResponseCode.OK, datas[0] - datas[1]);
+			case "multiply" ->  new Response(ResponseCode.OK, datas[0] * datas[1]);
+			case "divide" ->  new Response(ResponseCode.OK, datas[0] / datas[1]);		
+			default -> new Response(ResponseCode.WRONG_TYPE, request.requestType());		
+		};
+		};
+		return response;
+	}		
+	
 }
+
+	
